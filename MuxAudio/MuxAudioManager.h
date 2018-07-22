ae7f3847cab1d09352388fafd03adeb10282e0f3
Emiaostein <cszwdy@gmail.com>
2018-07-23 06:10:46 +0800

    Add resource.

---
+++ MuxAudio/MuxAudioManager.h
@@ -0,0 +1,15 @@
+//
+//  MuxAudioManager.h
+//  MuxAudio
+//
+//  Created by Emiaostein on 2018/7/20.
+//  Copyright © 2018 Emiaostein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MuxAudioManager : NSObject
+
+- (void)playAudioFileAt:(NSString *)path;
+
+@end
---
+++ MuxAudio/MuxAudioManager.m
@@ -0,0 +1,21 @@
+//
+//  MuxAudioManager.m
+//  MuxAudio
+//
+//  Created by Emiaostein on 2018/7/20.
+//  Copyright © 2018 Emiaostein. All rights reserved.
+//
+
+#import "MuxAudioManager.h"
+
+@implementation MuxAudioManager
+
+- (void)playAudioFileAt:(NSString *)path {
+    
+    // TODO: if mp3 then decode to PCM.
+    
+    
+
+}
+
+@end
